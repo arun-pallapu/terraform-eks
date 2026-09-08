a67d6a3f8bab3a9c41fc8cc484865bda955311ce
Ubuntu <ubuntu@ip-172-31-19-209.ec2.internal>
2026-09-08 13:01:41 +0000

    fix: use public subnets for EKS nodes to avoid private subnet connectivity issues

--- eks/main.tf
+++ eks/main.tf
@@ -124,7 +124,7 @@ module "eks" {
   # ----------------------------------------------------------
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc.public_subnets
 
   tags = local.tags
 }
