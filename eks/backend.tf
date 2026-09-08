60bdd66f492aec7f72e1cc000100dd2c6029d191
Ubuntu <ubuntu@ip-172-31-19-209.ec2.internal>
2026-09-08 09:44:22 +0000

    fix: remove use_lockfile unsupported in terraform 1.5.7

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-eks-state-arun-2026-0905"
-    key          = "eks/dev/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
+    bucket  = "terraform-eks-state-arun-2026-0905"
+    key     = "eks/dev/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
