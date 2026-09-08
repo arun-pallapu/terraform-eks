cb6b0034e8b83d7e43a52cddc1eb33246814033d
Ubuntu <ubuntu@ip-172-31-19-209.ec2.internal>
2026-09-08 12:10:51 +0000

    allow port 443 inbound from VPC CIDR to cluster security group

--- eks/main.tf
+++ eks/main.tf
@@ -126,6 +126,21 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  # ----------------------------------------------------------
+  # Cluster Security Group - allow nodes to reach API server
+  # ----------------------------------------------------------
+
+  cluster_security_group_additional_rules = {
+    ingress_nodes_443 = {
+      description = "Node groups to cluster API"
+      protocol    = "tcp"
+      from_port   = 443
+      to_port     = 443
+      type        = "ingress"
+      cidr_blocks = ["10.0.0.0/16"]
+    }
+  }
+
   tags = local.tags
 }
 
